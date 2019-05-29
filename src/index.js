9b3f8355fa2f3990b704a0de416064306315471b
Chris Abrams <mail@chrisabrams.com>
2019-05-29 08:24:07 -0400

    core files

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const { resolve } = require('path')
+const stripJsonComments = require('strip-json-comments')
+
+function ResolveTSPathsToAlias(options) {
+
+  const context = options.context
+  const json = fs.readFileSync(options.configFile, 'utf8')
+  const tsconfig = stripJsonComments(json)
+  const { paths } = JSON.parse(tsconfig).compilerOptions
+
+  const aliases = {}
+
+  Object.keys(paths).forEach((item) => {
+    const key = item.replace('/*', '')
+    const value = resolve(context, paths[item][0].replace('/*', '').replace('*', ''))
+
+    aliases[key] = value
+  })
+
+  return aliases
+
+}
+
+module.exports = ResolveTSPathsToAlias
